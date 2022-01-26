02bcc1b0963373032f5e03081b0608cc3e28982b
Jeffrey Wood <jwfirenze@gmail.com>
2022-01-26 11:45:13 -0500

    initial styling

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,26 +1,39 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
 /**
  * COMPONENT
  */
-export const Home = props => {
-  const {username} = props
-
+export const Home = () => {
   return (
-    <div>
-      <h3>Welcome, {username}</h3>
-    </div>
-  )
-}
+    <container className="container">
+      <video muted autoPlay="autoplay" loop>
+        <source src="waves.mp4" type="video/mp4"></source>
+      </video>
+      <div className="welcome">
+        <div>
+          <h3>Welcome to Lorem</h3>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+            aliquam nemo magnam assumenda, est asperiores dolorem dolor labore
+            enim voluptate.
+          </p>
+        </div>
+        <button type="button">Get Started</button>
+      </div>
+    </container>
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    username: state.auth.username
-  }
-}
+    username: state.auth.username,
+  };
+};
 
-export default connect(mapState)(Home)
+export default connect(mapState)(Home);
