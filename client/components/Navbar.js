ed53604494f2fb9638bb88a6dc247a28494bd8fa
Jeffrey Wood <jwfirenze@gmail.com>
2022-01-27 15:48:39 -0500

    navbar responsive

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,37 +2,62 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Navbar = () => {
   let [toggle, setToggle] = useState(false);
+
+  let menu = !toggle ? "menu" : "menu active";
 
   const clickHandler = (e) => {
     e.preventDefault();
     setToggle(!toggle ? true : false);
     e.target.className = !toggle ? "toggle active" : "toggle";
+    menu = !toggle ? "menu active" : "menu";
+  };
+
+  const closeHandler = () => {
+    setToggle(false);
   };
 
   return (
-    <div className="navbar">
-      <span className="header ml-2">
-        <a href="#top">Lorem</a>
-      </span>
-      <nav>
-        <div>
-          <a href="#auth">Components</a>
-          <a href="#getstarted">Text and Images</a>
-        </div>
-        <div>
-          <a href="#auth">
-            <button type="button">Sign Up</button>
-          </a>
-          <Link to="/">
-            <button type="button">Login</button>
-          </Link>
-        </div>
-      </nav>
-      <div className="toggle" onClick={clickHandler}></div>
-    </div>
+    <>
+      <div className="navbar">
+        <span className="header ml-2">
+          <a href="#top">Lorem</a>
+        </span>
+        <nav>
+          <div>
+            <a href="#auth">Components</a>
+            <a href="#getstarted">Text and Images</a>
+          </div>
+          <div>
+            <a href="#auth">
+              <button type="button">Sign Up</button>
+            </a>
+            <Link to="/">
+              <button type="button">Login</button>
+            </Link>
+          </div>
+        </nav>
+        <div className="toggle" onClick={clickHandler}></div>
+      </div>
+      <div className={menu}>
+        <ul>
+          <li onClick={closeHandler}>
+            <a href="#auth">Components</a>
+          </li>
+          <li onClick={closeHandler}>
+            <a href="#getstarted">Text and Images</a>
+          </li>
+          <li onClick={closeHandler}>
+            <a href="#auth">Sign Up</a>
+          </li>
+          <li onClick={closeHandler}>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 /**
