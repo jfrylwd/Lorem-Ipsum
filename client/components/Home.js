ac33dfd005546baa310d5592bc260086885f2c11
Jeffrey Wood <jwfirenze@gmail.com>
2022-01-27 13:30:37 -0500

    readme

--- client/components/Home.js
+++ client/components/Home.js
@@ -31,35 +31,33 @@ export const Home = () => {
         {/* <span className="scroll">Scroll down</span> */}
       </div>
       <span id="getstarted"></span>
-      <div className="content flex">
-        <img src="/nygold.jpeg" className="m-1 h-24 responsive" />
+      <div className="content flex where">
+        <div>
+          <img src="/nygold.jpeg" className="m-1 h-24" />
+        </div>
+        <div className="flex-col ml-2 block px-4">
+          <p className="header">Lorem, where things work, beautifully</p>
 
-        <div className="flex-col">
-          <p className="header ml-2 block px-4">
-            Lorem, where things work, beautifully
+          <p className="intro">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo
+            omnis, ullam laboriosam eaque aliquid sit reprehenderit ut quibusdam
+            molestiae impedit? Quasi quam autem impedit, excepturi cum eos
+            nostrum. Itaque aperiam culpa sequi adipisci. Itaque placeat non
+            natus! Qui, quis delectus? Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Vitae sit adipisci labore excepturi hic, suscipit
+            quibusdam inventore similique accusantium.Velit minus beatae
+            voluptatum, culpa natus eos possimus! Impedit unde dolorem in, animi
+            corrupti, consequatur laborum ipsum nisi est possimus obcaecati!
+            Error odio quas accusamus harum nam temporibus culpa! Recusandae et
+            numquam assumenda beatae odio porro quod dolores expedita ipsum
+            quam.
           </p>
-
-          <div className="ml-2 my-2 px-4">
-            <p className="intro">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo
-              omnis, ullam laboriosam eaque aliquid sit reprehenderit ut
-              quibusdam molestiae impedit? Quasi quam autem impedit, excepturi
-              cum eos nostrum. Itaque aperiam culpa sequi adipisci. Itaque
-              placeat non natus! Qui, quis delectus? Lorem, ipsum dolor sit amet
-              consectetur adipisicing elit. Vitae sit adipisci labore excepturi
-              hic, suscipit quibusdam inventore similique accusantium.Velit
-              minus beatae voluptatum, culpa natus eos possimus! Impedit unde
-              dolorem in, animi corrupti, consequatur laborum ipsum nisi est
-              possimus obcaecati! Error odio quas accusamus harum nam temporibus
-              culpa! Recusandae et numquam assumenda beatae odio porro quod
-              dolores expedita ipsum quam.
-            </p>
-            <p>
-              <button className="pill px-4 my-2">Create an Account</button>
-            </p>
-          </div>
+          <p>
+            <button className="pill px-4 my-2">Create an Account</button>
+          </p>
         </div>
       </div>
+
       {/* next view */}
       <div id="auth" className="content flex bg-lightgrey auth">
         <div className="px-4">
